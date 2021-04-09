b134af650c36424c65dadb205d4002c7decf6c6d
Gabsanity <65479530+Gabsanity@users.noreply.github.com>
2021-04-09 13:07:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -72,3 +72,36 @@ $(document).ready(function(){
         }
     });
 });
+
+
+//gabriel canlas animation//
+
+const text = document.querySelector('.text-2');
+const strText = text.textContent;
+const splitText = strText.split('');
+text.textContent = '';
+
+for(let i = 0; i < splitText.length; i++) {
+    text.innerHTML += '<span>' + splitText[i] + '</span>';
+
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+
+    if (char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+
